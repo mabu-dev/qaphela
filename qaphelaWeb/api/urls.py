c6aa6d3701692bbfe21581baf77bc0f39d688d58
Mabu Manaileng <Mabu.Manaileng@standardbank.co.za>
2020-10-11 20:30:35 +0200

    feature(api): cases and abuser endpoints

---
+++ qaphelaWeb/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from api import views as api_views
+
+urlpatterns = [
+    path('api/get-abuser/<str:full_names>', api_views.Abuser.as_view()),
+    path('api/case/<int:pk>', api_views.Case.as_view()),
+    path('api/abusers/', api_views.Abusers.as_view()),
+    path('api/cases/', api_views.Cases.as_view()),
+]
